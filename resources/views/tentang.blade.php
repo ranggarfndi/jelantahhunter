66444fc595b3aad10955197b3b21eb3e255eefed
unknown <ranggarafandi010703@gmail.com>
2024-11-10 13:41:24 +0700

    ubah tampilan tentang sikit

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="my-5 pt-20 pb-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="">
                     <!-- Title Section -->
-                    <h1 class="text-center text-5xl text-orange-500 font-medium mb-20 mt-20">
+                    <h1 class="text-center text-5xl text-orange-500 font-medium my-16">
                         Tentang Kami
                     </h1>
 
